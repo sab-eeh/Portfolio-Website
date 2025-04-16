4544472a3312cbc30a8709c176d8b102b705b27f
sab-eeh <usabeeh72@gmail.com>
2025-04-16 18:55:58 +0500

    changed file structure

--- app/Frontend/api/contact.js
+++ app/Frontend/api/contact.js
@@ -1,5 +1,5 @@
-import { connectToDatabase, FormModel } from "./db";
-import { transporter } from "./email";
+import { connectToDatabase, FormModel } from "../db";
+import { transporter } from "../email";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
